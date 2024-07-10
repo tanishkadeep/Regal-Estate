d69a4dab5a9065e74d556bed038e579e9035aacf
Tanishka <tanishkadeep09@gmail.com>
2024-07-10 14:19:31 +0530

    added redux persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>{" "}
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>{" "}
+    </PersistGate>
   </Provider>
 );
