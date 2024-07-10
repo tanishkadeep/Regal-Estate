9419437a0039034d5967f39168248c3ecc133328
Tanishka <tanishkadeep09@gmail.com>
2024-07-10 09:35:43 +0530

    styling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl p-3 mx-auto">
+      <div className="flex justify-between items-center max-w-6xl py-3 mx-auto px-3 sm:px-10 gap-2">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl">
+          <h1 className="font-bold text-lg sm:text-2xl">
             <span className="text-slate-500">Regal</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form className="bg-slate-100 py-3 px-2 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
@@ -19,19 +19,22 @@ export default function Header() {
           />
           <FaSearch className="text-slate-600" />
         </form>
-        <ul className="flex gap-4">
+
+        <ul className="flex gap-4 font-medium">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden md:inline text-slate-700 hover:underline">
               Home
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden md:inline text-slate-700 hover:underline">
               About
             </li>
           </Link>
           <Link to="/signin">
-            <li className=" text-slate-700 hover:underline">Sign in</li>
+            <li className=" text-slate-700 hover:underline text-center">
+              Login
+            </li>
           </Link>
         </ul>
       </div>
