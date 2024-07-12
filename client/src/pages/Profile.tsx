c4419d4af5cb97d66b5ea5df673c52c041bb7b1b
Tanishka <tanishkadeep09@gmail.com>
2024-07-13 00:55:19 +0530

    show user listings functionality

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -31,6 +31,26 @@ export default function Profile() {
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState<Listing[]>([]);
+
+  interface Listing {
+    name: String;
+    description: String;
+    address: Listing;
+    regularPrice: Number;
+    discountPrice: Number;
+    bathrooms: Number;
+    bedrooms: Number;
+    furnished: Boolean;
+    parking: Boolean;
+    type: String;
+    offer: Boolean;
+    imageUrls: Array<string>;
+    userRef: String;
+    _id: String;
+  }
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -124,8 +144,26 @@ export default function Profile() {
     }
   };
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+
+      const res = await fetch(`/api/user/listings/${currentUser?.id}`);
+
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
   return (
-    <div className="p-3 max-w-lg mx-auto">
+    <div className="p-3 max-w-lg mx-auto mb-16">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
@@ -226,9 +264,49 @@ export default function Profile() {
       <p className="text-red-700 mt-5 text-center font-semibold">
         {error ? error : ""}
       </p>
+
       <p className="text-green-700 mt-5 text-center font-semibold">
         {updateSuccess ? "User is updated successfully!" : ""}
       </p>
+
+      <button
+        onClick={handleShowListings}
+        className="text-green-700 w-full font-semibold text-lg hover:underline mt-6"
+      >
+        Show Listings
+      </button>
+
+      <p className="text-red-700 mt-5 text-center font-medium">
+        {showListingsError ? "Error showing listings" : ""}
+      </p>
+
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-bold">Your Listings</h1>
+          {userListings.map((listing) => (
+            <div className="border rounded-lg p-3 flex justify-between items-center gap-4 ">
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p className="text-lg">{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col item-center font-medium">
+                <button className="text-red-700 uppercase ">Delete</button>
+                <button className="text-green-700 uppercase ">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
