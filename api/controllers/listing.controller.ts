c968a02a7eb8db55d3c0b77dceec75250f90d0d6
Tanishka <tanishkadeep09@gmail.com>
2024-07-17 00:14:07 +0530

    create search api route

--- api/controllers/listing.controller.ts
+++ api/controllers/listing.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import Listing from "../models/listing.model";
-import { z } from "zod";
+import { string, z } from "zod";
 import { errorHandler } from "../utils/error";
+import { SortOrder } from "mongoose";
 
 const listingSchema = z.object({
   name: z.string(),
@@ -103,4 +104,53 @@ const getListing = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { createListing, deleteListing, updateListing, getListing };
+const getListings = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const limit: number = parseInt(req.query.limit as string) || 9;
+    const startIndex = parseInt(req.query.startIndex as string) || 0;
+
+    const offerParam = req.query.offer as string | undefined;
+    const furnishedParam = req.query.furnished as string | undefined;
+    const parkingParam = req.query.parking as string | undefined;
+    const typeParam = req.query.type as string | undefined;
+    const searchTerm = (req.query.searchTerm as string) || "";
+    const sort = (req.query.sort as string) || "createdAt";
+    const order: SortOrder = (req.query.order as "asc" | "desc") || "desc";
+
+    const offer =
+      offerParam === undefined || offerParam === "false"
+        ? { $in: [false, true] }
+        : offerParam === "true";
+
+    const furnished =
+      furnishedParam === undefined || furnishedParam === "false"
+        ? { $in: [false, true] }
+        : furnishedParam === "true";
+
+    const parking =
+      parkingParam === undefined || parkingParam === "false"
+        ? { $in: [false, true] }
+        : parkingParam === "true";
+    const type =
+      typeParam === undefined || typeParam === "all"
+        ? { $in: ["sale", "rent"] }
+        : typeParam;
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createListing, deleteListing, updateListing, getListing, getListings };
