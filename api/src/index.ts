21987dd9a2788f96ba92679031587c83957b7b2f
Tanishka <tanishkadeep09@gmail.com>
2024-07-17 22:38:01 +0530

    bug

--- api/src/index.ts
+++ api/src/index.ts
@@ -32,7 +32,7 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
