6a06a751352c8eb4ad7e5c7a1df13ff707d8f179
Tanishka <tanishkadeep09@gmail.com>
2024-07-17 23:03:57 +0530

    render

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://regal-estates.onrender.com",
         secure: false,
       },
     },
