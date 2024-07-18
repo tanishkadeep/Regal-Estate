f2a3995c8ccec1d3c0531166c0e2eb1af6d424da
Tanishka Deep <115184621+tanishkadeep@users.noreply.github.com>
2024-07-18 20:39:45 +0530

    minor change

--- client/src/components/ListingItem.tsx
+++ client/src/components/ListingItem.tsx
@@ -8,7 +8,7 @@ interface ContactProps {
 
 export default function ListingItem({ listing }: ContactProps) {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full md:w-[330px]">
       <Link to={`/listing/${listing._id}`}>
         <img
           src={
@@ -16,7 +16,7 @@ export default function ListingItem({ listing }: ContactProps) {
             "https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg"
           }
           alt="listing cover"
-          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
+          className="h-[320px] md:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
         <div className="p-3 flex flex-col gap-2 w-full">
           <p className="truncate text-lg font-bold text-slate-700">
